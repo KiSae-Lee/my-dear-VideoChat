12d9648968c7ec493a8c64b1fce6bfdbf852c6ed
Dokina <dokina@Dokinas-MacBook-Pro.local>
2022-10-10 17:30:16 +0900

    console chatting.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,23 @@
-alert("Welcome to my-dear-VideoChat!");
+const msgList = document.querySelector("ul");
+const msgForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open", () => {
+  console.log("Connected to server.");
+});
+
+socket.addEventListener("message", (event) => {
+  console.log(event.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server.");
+});
+
+setTimeout(() => socket.send("Hi"), 5000);
+
+msgForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = msgForm.querySelector("input");
+  socket.send(input.value);
+});
--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ app.set("views", __dirname + "/view"); // tell where is view files.
 
 app.use("/public", express.static(__dirname + "/public")); // where users can access.
 
-app.get("/", (req, res) => res.render("home")); // pug file for render.
-app.get("/*", (req, res) => res.redirect("/")); // if user want to be another url. send them back to home.
+app.get("/", (_, res) => res.render("home")); // pug file for render.
+app.get("/*", (_, res) => res.redirect("/")); // if user want to be another url. send them back to home.
 
 // Using http protocol for debug.
 const handleListen = () => console.log("Listening to https://localhost:3000");
@@ -20,5 +20,25 @@ const server = http.createServer(app); // create http server.
 const wss = new Websocket.Server({ server }); // create websocket server.
 
 // now, you can handle both of servers through https://localhost:3000.
+
+// anonymous function.
+// (var1, var2, ...) => { "your function here." }
+
+// fake DB.
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  // connection msgs.
+  console.log("Connected to the browser.");
+  socket.on("close", () => {
+    console.log("Disconnected from the browser.");
+  });
+
+  // chatting.
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+});
 
 server.listen(3000, handleListen);
